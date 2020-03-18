774467681872e4b3727529d1a5cdf469ba685cfb
Kevin <kevinpanacci@hotmail.it>
2020-03-18 14:04:09 +0100

    Aggiunta funzione ceil per il voto

--- js/main.js
+++ js/main.js
@@ -1,34 +1,49 @@
 $(document).ready(function (){
     $('.searchbar-button').click(function() {
         var nomeInserito = $('#searchbar').val();
-        console.log(nomeInserito);
-    });
+        // console.log(nomeInserito);
+        var apiBaseUrl = 'https://api.themoviedb.org/3';
 
-    var nome = $('#searchbar').val();
-    console.log(nome); //COSI FUNZIONA
+        $.ajax({
+            url: apiBaseUrl + '/search/movie',
+            data: {
+                api_key: '6d0fd5c96501c78452d6d55bbbef0583',
+                query: nomeInserito,
+                language: 'it-IT'
+            },
+            method: 'GET',
+            success: function(data){
+                console.log(data);
+                var films = data.results;
+                for (var i = 0; i < films.length; i++) {
+                    var film = films[i];
+                    console.log(film.title);
+                    console.log(film.original_title);
+                    console.log(film.original_language);
+                    console.log(film.vote_average);
+                    var votoInDecimi = film.vote_average;
+                    votoStelle(votoInDecimi);
 
 
-    var apiBaseUrl = 'https://api.themoviedb.org/3';
-    $.ajax({
-        url: apiBaseUrl + '/search/movie',
-        data: {
-            api_key: '6d0fd5c96501c78452d6d55bbbef0583',
-            query: nome,
-            language: 'it-IT'
-        },
-        method: 'GET',
-        success: function(data){
-            console.log(data);
-            var films = data.results;
-            for (var i = 0; i < films.length; i++) {
-                var film = films[i];
-                console.log(film.title);
+
+
+
+                    function votoStelle(votoInDecimi) {
+                        var stelle = Math.ceil(votoInDecimi / 2);
+                        console.log(stelle);
+                    }
+                }
+            },
+            error: function (err) {
+                alert('BOOM');
             }
-        },
-        error: function (err) {
-            alert('BOOM');
-        }
+        });
+
     });
+
+
+
+
 
 
 });
