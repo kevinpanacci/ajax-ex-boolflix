ff4773e81cb1268fe596042e3cdac8b3b30aaa5c
Kevin <kevinpanacci@hotmail.it>
2020-03-18 16:19:50 +0100

    Aggiunto metodo stelle

--- js/main.js
+++ js/main.js
@@ -25,13 +25,12 @@ $(document).ready(function (){
                     votoStelle(votoInDecimi);
 
 
-
-
-
-
                     function votoStelle(votoInDecimi) {
-                        var stelle = Math.ceil(votoInDecimi / 2)%;
+                        var stelle = Math.ceil(votoInDecimi / 2);
                         console.log(stelle);
+                        var stellePercentuale = ((stelle / 5) * 100);
+                        console.log(stellePercentuale);
+                        $('.stars-inner').css('width', 'stellePercentuale' + '%' )
                     }
                 }
             },
@@ -41,27 +40,4 @@ $(document).ready(function (){
         });
 
     });
-
-
-//     DA STUDIARE PER UTILIZZARE LE STELLE CON %.
-//     const ratings = {
-//       hotel_a : 2.8,
-//       hotel_b : 3.3,
-//       hotel_c : 1.9,
-//       hotel_d : 4.3,
-//       hotel_e : 4.74
-//     };
-//
-//     const starTotal = 5;
-//
-// for(const rating in ratings) {
-//   // 2
-//   const starPercentage = (ratings[rating] / starTotal) * 100;
-//   // 3
-//   const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
-//   // 4
-//   document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
-// }
-
-
 });
