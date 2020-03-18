3c2adf1118192de6aec0a90f8d7aaced72e899fc
Kevin <kevinpanacci@hotmail.it>
2020-03-18 22:31:19 +0100

    Rimossa bandierina aggiunto template

--- js/main.js
+++ js/main.js
@@ -1,18 +1,8 @@
 $(document).ready(function (){
-
-    // const parentElement = document.getElementById("bandiera");
-    // const flag = new CountryFlag(parentElement);
-    // miaBandiera = 'it';
-    // flag.selectByTopLevelDomain(miaBandiera);
-
-    $('.card').mouseenter(function(){
-        $('.image-container').hide();
-        $('.overlay').fadeIn();
-
-    });
+var apiBaseUrl = 'https://api.themoviedb.org/3';
     $('.searchbar-button').click(function() {
         var nomeInserito = $('#searchbar').val();
-        var apiBaseUrl = 'https://api.themoviedb.org/3';
+
 
 // CHIAMATA ALL'API PER I FILM
         $.ajax({
@@ -27,46 +17,16 @@ $(document).ready(function (){
             success: function(data){
                 console.log(data);
                 var films = data.results;
-                cicloFilm(films);
-
-                    function cicloFilm(listaFilm) {
-                        for (var i = 0; i < films.length; i++) {
-                            var film = listaFilm[i];
-                            console.log(film.title);
-                            console.log(film.original_title);
-                            console.log(film.original_language);
-                            console.log(film.vote_average);
-                            console.log(film.poster_path);
-                            var titoloIta = film.title;
-                            var titoloOriginale = film.original_title;
-                            var posterPath = film.poster_path;
-                            var votoInDecimi = film.vote_average;
-                            var dimensioneImmagine = 'w154';
-                            var urlImmagine = "https://image.tmdb.org/t/p/" + dimensioneImmagine + posterPath;
-                            console.log(urlImmagine);
-                            votoStelle(votoInDecimi);
-                            creaBandiera();
-                            function creaBandiera() {
-                                var miaBandiera = film.original_language;
-                                const parentElement = document.getElementById("bandiera");
-                                const flag = new CountryFlag(parentElement);
-                                flag.selectByTopLevelDomain(miaBandiera);
-                                if (miaBandiera == 'en') {
-                                    flag.selectByTopLevelDomain('uk');
-                                }
-                            }
-                            function votoStelle(votoInDecimi) {
-                                var stelle = Math.ceil(votoInDecimi / 2);
-                                var stellePercentuale = ((stelle / 5) * 100);
-                                $('.stars-inner').css('width', stellePercentuale +'%')
-                            }
-                    }
-                }
+                var source = $('#card-template').html();
+                var cardTemplate = Handlebars.compile(source);
+                cicloFilm(films,cardTemplate);
             },
             error: function (err) {
                 alert('BOOM');
             }
         });
+
+
 
 // CHIAMATA ALL'API PER LE SERIE TV
         $.ajax({
@@ -81,45 +41,89 @@ $(document).ready(function (){
                 console.log(data);
                 var series = data.results;
                 cicloSerie(series);
-
-                    function cicloSerie(listaFilm) {
-                        for (var i = 0; i < listaFilm.length; i++) {
-                            var film = listaFilm[i];
-                            console.log(film.name);
-                            console.log(film.original_name);
-                            console.log(film.original_language);
-                            console.log(film.vote_average);
-                            console.log(film.poster_path);
-                            var votoInDecimi = film.vote_average;
-                            var posterPath = film.poster_path;
-                            var titoloIta = film.name;
-                            var titoloOriginale = film.original_name;
-                            var dimensioneImmagine = 'w342';
-                            var urlImmagine = "https://image.tmdb.org/t/p/" + dimensioneImmagine + posterPath;
-                            votoStelle(votoInDecimi);
-                            creaBandiera();
-                            function creaBandiera() {
-                                var miaBandiera = film.original_language;
-                                const parentElement = document.getElementById("bandiera");
-                                const flag = new CountryFlag(parentElement);
-                                flag.selectByTopLevelDomain(miaBandiera);
-                                if (miaBandiera == 'en') {
-                                    flag.selectByTopLevelDomain('uk');
-                                }
-                            }
-                            function votoStelle(votoInDecimi) {
-                                var stelle = Math.ceil(votoInDecimi / 2);
-                                var stellePercentuale = ((stelle / 5) * 100);
-                                $('.stars-inner').css('width', stellePercentuale +'%')
-                            }
-                    }
-
-                }
+                var source = $('#card-template').html();
+                var cardTemplate = Handlebars.compile(source);
             },
             error: function (err) {
                 alert('BOOM');
             }
         });
-
     });
+    // $('.card').mouseenter(function(){
+    //     $('.image-container').hide();
+    //     $('.overlay').fadeIn();
+    // });
 });
+function attivax(el,scelta){
+    if (scelta) {
+        el.find('.image-container').hide();
+        el.find('.overlay').show();
+    } else {
+        el.find('.image-container').show();
+        el.find('.overlay').hide();
+    }
+
+}
+function cicloSerie(listaFilm) {
+    for (var i = 0; i < listaFilm.length; i++) {
+        var film = listaFilm[i];
+        var votoInDecimi = film.vote_average;
+        var posterPath = film.poster_path;
+        var titoloIta = film.name;
+        var titoloOriginale = film.original_name;
+        var overview = film.overview;
+        var dimensioneImmagine = 'w342';
+        var urlImmagine = "https://image.tmdb.org/t/p/" + dimensioneImmagine + posterPath;
+        var voto2=votoStelle2(votoInDecimi);
+
+        // creaBandiera();
+        // function creaBandiera() {
+        //     var miaBandiera = film.original_language;
+        //     const parentElement = document.getElementById("bandiera");
+        //     const flag = new CountryFlag(parentElement);
+        //     flag.selectByTopLevelDomain(miaBandiera);
+        //     if (miaBandiera == 'en') {
+        //         flag.selectByTopLevelDomain('uk');
+        //     }
+        // }
+    }
+}
+
+function cicloFilm(listaFilm,cardTemplate) {
+    for (var i = 0; i < listaFilm.length; i++) {
+        var obj={};
+        var film = listaFilm[i];
+        obj.titoloIta = film.title;
+        obj.titoloOriginale = film.original_title;
+        var posterPath = film.poster_path;
+        var votoInDecimi = film.vote_average;
+        obj.overview = film.overview;
+        var dimensioneImmagine = 'w154';
+        obj.urlImmagine = "https://image.tmdb.org/t/p/" + dimensioneImmagine + posterPath;
+        obj.voto2=votoStelle2(votoInDecimi);
+        // creaBandiera();
+        var html = cardTemplate(obj);
+        $('.container-card').append(html);
+
+        // function creaBandiera() {
+        //     var miaBandiera = film.original_language;
+        //     const parentElement = document.getElementById("bandiera");
+        //     const flag = new CountryFlag(parentElement);
+        //     flag.selectByTopLevelDomain(miaBandiera);
+        //     if (miaBandiera == 'en') {
+        //         flag.selectByTopLevelDomain('uk');
+        //     }
+        // }
+        // function votoStelle(votoInDecimi) {
+        //     var stelle = Math.ceil(votoInDecimi / 2);
+        //     var stellePercentuale = ((stelle / 5) * 100);
+        //     $('.stars-inner').css('width', stellePercentuale +'%')
+        // }
+    }
+}
+
+function votoStelle2(votoInDecimi) {
+    var stelle = Math.ceil(votoInDecimi / 2);
+    var stellePercentuale = ((stelle / 5) * 100);
+    return stellePercentuale;
+}
