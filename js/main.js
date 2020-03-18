b4061e58d35f40afc891b1a7e9a94f8304b256a4
Kevin <kevinpanacci@hotmail.it>
2020-03-18 17:06:29 +0100

    Aggiunta call serie tv

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ $(document).ready(function (){
         var nomeInserito = $('#searchbar').val();
         // console.log(nomeInserito);
         var apiBaseUrl = 'https://api.themoviedb.org/3';
-
+// CHIAMATA ALL'API PER I FILM
         $.ajax({
             url: apiBaseUrl + '/search/movie',
+            // url: apiBaseUrl + '/search/tv',
             data: {
                 api_key: '6d0fd5c96501c78452d6d55bbbef0583',
                 query: nomeInserito,
@@ -15,22 +16,62 @@ $(document).ready(function (){
             success: function(data){
                 console.log(data);
                 var films = data.results;
-                for (var i = 0; i < films.length; i++) {
-                    var film = films[i];
-                    console.log(film.title);
-                    console.log(film.original_title);
-                    console.log(film.original_language);
-                    console.log(film.vote_average);
-                    var votoInDecimi = film.vote_average;
-                    votoStelle(votoInDecimi);
+                cicloFilm(films);
 
-
+                    function cicloFilm(listaFilm) {
+                        for (var i = 0; i < films.length; i++) {
+                            var film = listaFilm[i];
+                            console.log(film.title);
+                            console.log(film.original_title);
+                            console.log(film.original_language);
+                            console.log(film.vote_average);
+                            var votoInDecimi = film.vote_average;
+                            votoStelle(votoInDecimi);
+                    }
                     function votoStelle(votoInDecimi) {
                         var stelle = Math.ceil(votoInDecimi / 2);
                         console.log(stelle);
                         var stellePercentuale = ((stelle / 5) * 100);
                         console.log(stellePercentuale);
-                        $('.stars-inner').css('width', 'stellePercentuale' + '%' )
+                        $('.stars-inner').css('width', stellePercentuale +'%')
+                    }
+                }
+            },
+            error: function (err) {
+                alert('BOOM');
+            }
+        });
+
+// CHIAMATA ALL'API PER LE SERIE TV
+        $.ajax({
+            url: apiBaseUrl + '/search/tv',
+            data: {
+                api_key: '6d0fd5c96501c78452d6d55bbbef0583',
+                query: nomeInserito,
+                language: 'it-IT'
+            },
+            method: 'GET',
+            success: function(data){
+                console.log(data);
+                var series = data.results;
+                cicloSerie(series);
+
+                    function cicloSerie(listaFilm) {
+                        for (var i = 0; i < listaFilm.length; i++) {
+                            var film = listaFilm[i];
+                            console.log(film.name);
+                            console.log(film.original_name);
+                            console.log(film.original_language);
+                            console.log(film.vote_average);
+                            var votoInDecimi = film.vote_average;
+                            votoStelle(votoInDecimi);
+                    }
+                    function votoStelle(votoInDecimi) {
+                        var stelle = Math.ceil(votoInDecimi / 2);
+                        console.log(stelle);
+                        var stellePercentuale = ((stelle / 5) * 100);
+                        console.log(stellePercentuale);
+                        $('.stars-inner').css('width', stellePercentuale +'%')
                     }
                 }
             },
