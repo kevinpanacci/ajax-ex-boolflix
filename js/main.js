81986529090a5ba4e577dcb52bcbb2e0658e1a3b
Kevin <kevinpanacci@hotmail.it>
2020-03-18 14:32:17 +0100

    Aggiunto su js codice riguardante stelle da studiare

--- js/main.js
+++ js/main.js
@@ -28,8 +28,9 @@ $(document).ready(function (){
 
 
 
+
                     function votoStelle(votoInDecimi) {
-                        var stelle = Math.ceil(votoInDecimi / 2);
+                        var stelle = Math.ceil(votoInDecimi / 2)%;
                         console.log(stelle);
                     }
                 }
@@ -42,8 +43,25 @@ $(document).ready(function (){
     });
 
 
-
-
+//     DA STUDIARE PER UTILIZZARE LE STELLE CON %.
+//     const ratings = {
+//       hotel_a : 2.8,
+//       hotel_b : 3.3,
+//       hotel_c : 1.9,
+//       hotel_d : 4.3,
+//       hotel_e : 4.74
+//     };
+//
+//     const starTotal = 5;
+//
+// for(const rating in ratings) {
+//   // 2
+//   const starPercentage = (ratings[rating] / starTotal) * 100;
+//   // 3
+//   const starPercentageRounded = `${(Math.round(starPercentage / 10) * 10)}%`;
+//   // 4
+//   document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+// }
 
 
 });
